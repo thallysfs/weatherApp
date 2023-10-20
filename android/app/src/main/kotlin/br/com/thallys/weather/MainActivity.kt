eec0f0c0e298ff6a62e24314f395ac12128f1bb9
thallysfs <thallys@hotmail.com>
2023-10-20 08:03:17 -0300

    feat: new app weather app

---
+++ android/app/src/main/kotlin/br/com/thallys/weather/MainActivity.kt
@@ -0,0 +1,6 @@
+package br.com.thallys.weather
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/widgets/secrets.dart
@@ -0,0 +1,1 @@
+const openWeatherAPIKey = 'd7d0f21380533223088f6dc08e797b4b';
